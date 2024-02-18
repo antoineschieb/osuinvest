1e0e1049a978dac7e41add9a7069b50e27006d31
Antoine Schieb <antoine.schieb@u-bourgogne.fr>
2024-02-18 03:08:34 +0100

    first version

---
+++ utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+from rules import TAX
+
+
+def valuate(stock):
+    available_shares = stock.total_shares - stock.sold_shares
+    supply_demand_ratio = (stock.total_shares + stock.sold_shares)/(available_shares+0.001)
+    intrinsic_value = stock.raw_skill * pow(stock.trendiness, stock.prestige)
+    return round(supply_demand_ratio * intrinsic_value,2)
+
+
+def get_stock_by_name(name: str) -> pd.Series:
+    df_s = pd.read_csv("all_stocks_static.csv", index_col='name')
+    df_d = pd.read_csv("all_stocks_dynamic.csv", index_col='name')
+    x_s = df_s.loc[name,:]
+    x_d = df_d.loc[name,:]
+    ret = pd.concat([x_s, x_d])
+    return ret
+
+
+def get_investor_by_name(name: str) -> pd.Series:
+    df = pd.read_csv("all_investors.csv", index_col='name')
+    x = df.loc[name,:]
+    return x
+
+
+def get_portfolio(buyer_name: str) -> pd.DataFrame:
+    buyer_portfolio = pd.read_csv(f'portfolios/{buyer_name}.csv', index_col='stock_name')
+    return buyer_portfolio
+
+
+def get_net_worth(investor_name: str) -> float:
+    investor = get_investor_by_name(investor_name)
+    net_worth = investor.cash_balance
+    portfolio = get_portfolio(investor_name)
+    for s in portfolio.index:
+        qty = portfolio.loc[s,'shares_owned']
+        stock = get_stock_by_name(s)
+        net_worth += qty * valuate(stock)    #TODO: find smarter way to adjust for tax? 
+    return net_worth
+
+def get_dividend_return(stock_name) -> float:
+    stock = get_stock_by_name(stock_name)
+    return stock.prestige - 1
