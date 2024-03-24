93979f1b157dffc06ab2854dd35e194d4cc14aab
Antoine Schieb <antoine.schieb@cpe.fr>
2024-03-24 13:56:44 +0100

    drop na on stock prices history

--- client_loop.py
+++ client_loop.py
@@ -62,6 +62,7 @@ async def update_static_stats():
         
         # update stock prices
         df_updates = pd.concat([df_updates, df_updates_appendice])
+        df_updates = df_updates.dropna(axis=0)
         df_updates.to_csv(f"{SEASON_ID}/stock_prices_history.csv", index='update_id')
 
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Done!")
